1702359efb8c797944890c4cdb15b27c3abff11a
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-17 03:49:26 +0530

    feat : cart context

---
+++ src/contexts/cart-context.js
@@ -0,0 +1,17 @@
+import { createContext, useContext, useReducer } from "react";
+import { cartReducer } from "../reducers/cart-reducer";
+
+const cartContext = createContext();
+
+const CartContextProvider = ({ children }) => {
+  const [cartState, cartDispatch] = useReducer(cartReducer, []);
+  return (
+    <cartContext.Provider value={{ cartState, cartDispatch }}>
+      {children}
+    </cartContext.Provider>
+  );
+};
+
+const useCartContext = () => useContext(cartContext);
+
+export { CartContextProvider, useCartContext };
---
+++ src/reducers/cart-reducer.js
@@ -0,0 +1,3 @@
+const cartReducer = () => {};
+
+export { cartReducer };

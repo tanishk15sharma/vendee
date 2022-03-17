eb16861bc4846069f54e44b3b5cd9f458bce4d72
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-17 12:50:26 +0530

    feat : wishlist reducer initialized

--- src/contexts/wishlist-context.js
+++ src/contexts/wishlist-context.js
@@ -1,9 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { wishlistReducer } from "../reducers/wishlist-reducer";
 
 const wishlistContext = createContext();
 
 const WishlistContextProvider = ({ children }) => {
-  return <wishlistContext.Provider>{children}</wishlistContext.Provider>;
+  const [wishlistState, wishlistDispatch] = useReducer(wishlistReducer, []);
+  return (
+    <wishlistContext.Provider value={{ wishlistState, wishlistDispatch }}>
+      {children}
+    </wishlistContext.Provider>
+  );
 };
 
 const useWishlist = () => useContext(wishlistContext);
---
+++ src/reducers/wishlist-reducer.js
@@ -0,0 +1,3 @@
+const wishlistReducer = () => {};
+
+export { wishlistReducer };

15971b02a8273703238f04d317312150389a0f82
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-22 14:28:27 +0530

    feat : signup validations

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -2,88 +2,106 @@ import "./SignUp.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "../../contexts/auth-context";
+import { validSignUp } from "../../utilities/auth";
 
 const SignUp = () => {
   const { authState, authDispatch } = useAuth();
 
-  const [userInfo, setUserInfo] = useState({
-    firstname: "",
-    secondname: "",
+  const [signUpData, setSignUpData] = useState({
+    firstName: "",
+    lastName: "",
     mobile: "",
     email: "",
     password: "",
+    confirmPassword: "",
+  });
+
+  const [signUpErrors, setSignUpErrors] = useState({
+    firstName: "",
+    lastName: "",
+    mobile: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
   const inputHandler = (e) => {
-    e.preventDefault();
-    setUserInfo((data) => ({ ...data, [e.target.name]: e.target.value }));
+    setSignUpData((data) => ({ ...data, [e.target.name]: e.target.value }));
+    setSignUpErrors((errData) => ({
+      ...errData,
+      [e.target.name]: "",
+    }));
   };
-  const postSignUpDetails = async (e) => {
-    e.preventDefault();
-    authDispatch({ type: "USER_LOAD" });
 
+  console.log(signUpData);
+  const postSignUpDetails = async (e) => {
     try {
-      const { data } = await axios.post("/api/auth/signup", userInfo);
+      e.preventDefault();
+      const { isValid, errors } = validSignUp(signUpData, signUpErrors);
+      authDispatch({ type: "USER_LOAD" });
+      const { data } = await axios.post("/api/auth/signup", signUpData);
       authDispatch({ type: "USER_LOAD_SUCCESS", payload: data.createdUser });
       console.log(data);
       localStorage.setItem("token", data.encodedToken);
     } catch (err) {
       console.log(err);
     }
-    // console.log(authState);
   };
-
-  useEffect(() => {
-    console.log(authState);
-  });
 
   return (
     <main className="center">
-      <form class="login-container" onSubmit={postSignUpDetails}>
-        <h1 class="login-title">Sign up</h1>
+      <form className="login-container" onSubmit={postSignUpDetails}>
+        <h1 className="login-title">Sign up</h1>
         <input
           type="text"
           placeholder="First Name"
-          name="firstname"
-          value={userInfo.firstname}
+          name="firstName"
+          value={signUpData.firstName}
           onChange={inputHandler}
         />
         <input
           type="text"
-          placeholder="Second Name"
-          name="secondname"
-          value={userInfo.secondname}
+          placeholder="Last Name"
+          name="lastName"
+          value={signUpData.lastName}
           onChange={inputHandler}
         />
         <input
           type="text"
           placeholder="Mobile"
           name="mobile"
-          value={userInfo.mobile}
+          value={signUpData.mobile}
           onChange={inputHandler}
         />
         <input
           type="text"
           placeholder="Email"
           name="email"
-          value={userInfo.email}
+          value={signUpData.email}
           onChange={inputHandler}
         />
         <input
           type="text"
-          placeholder="Password`"
+          placeholder="Password"
           name="password"
-          value={userInfo.password}
+          value={signUpData.password}
           onChange={inputHandler}
         />
-        <div class="signup-options">
+        <input
+          type="text"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          value={signUpData.confirmPassword}
+          onChange={inputHandler}
+        />
+        <div className="signup-options">
           <div>
             <input type="checkbox" />
 
-            <label for="remember-me">I accept all Terms & Conditions</label>
+            <label htmlFor="remember-me">I accept all Terms & Conditions</label>
           </div>
         </div>
-        <button class="login-btn" type="submit">
+        <button className="login-btn" type="submit">
           Creat New Account
         </button>
       </form>
--- src/utilities/auth.js
+++ src/utilities/auth.js
@@ -1,3 +1,11 @@
+import {
+  EMAIL_REGEX,
+  PASSWORD_LOWERCASE_REGEX,
+  PASSWORD_UPPERCASE_REGEX,
+  PASSWORD_NUMBER_REGEX,
+  MOBILE_NUMBER_REGEX,
+} from "./regexp";
+
 const validLogin = ({ email, password }, initialErrors) => {
   if (!email)
     return {
@@ -24,4 +32,90 @@ const validLogin = ({ email, password }, initialErrors) => {
   };
 };
 
-export { validLogin };
+const validSignUp = (
+  { firstName, lastName, mobile, email, password, confirmPassword },
+  initialErrors
+) => {
+  if (!firstName)
+    return {
+      isValid: false,
+      errors: { ...initialErrors, firstName: "First Name is Required" },
+    };
+  if (!lastName)
+    return {
+      isValid: false,
+      errors: { ...initialErrors, lastName: "Last Name is Required" },
+    };
+  if (!mobile)
+    return {
+      isValid: false,
+      errors: { ...initialErrors, mobile: "Mobile Number is Required" },
+    };
+  if (!email)
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        email: "Email is Required",
+      },
+    };
+  if (!EMAIL_REGEX.test(email))
+    return {
+      isValid: false,
+      errors: { ...initialErrors, email: "Enter a valid Email Address" },
+    };
+  if (!MOBILE_NUMBER_REGEX.test(mobile))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        mobile: "Mobile Number should only contain Numbers",
+      },
+    };
+  if (!PASSWORD_UPPERCASE_REGEX.test(password))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        password: "Password must contain atleast 1 Uppercase Letter",
+      },
+    };
+  if (!PASSWORD_LOWERCASE_REGEX.test(password))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        password: "Password must contain atleast 1 Lowercase Letter",
+      },
+    };
+  if (!PASSWORD_NUMBER_REGEX.test(password))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        password: "Password must contain atleast 1 number ",
+      },
+    };
+  if (password !== confirmPassword)
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        confirmPassword: "Password does not match",
+      },
+    };
+
+  return {
+    isValid: true,
+    errors: {
+      firstName: "",
+      lastName: "",
+      mobile: "",
+      password: "",
+      confirmPassword: "",
+      email: "",
+    },
+  };
+};
+
+export { validLogin, validSignUp };

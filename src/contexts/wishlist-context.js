fc0d6247fca3187da54e273dc79d94d2db136ce0
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-23 16:01:14 +0530

    feat : wsihlist data from API

--- src/contexts/wishlist-context.js
+++ src/contexts/wishlist-context.js
@@ -1,9 +1,25 @@
-import { createContext, useContext, useReducer } from "react";
+import axios from "axios";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import { wishlistReducer } from "../reducers/wishlist-reducer";
 
 const wishlistContext = createContext();
 
 const WishlistContextProvider = ({ children }) => {
+  const encodedToken = localStorage.getItem("token");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("/api/user/wishlist", {
+          headers: { authorization: encodedToken },
+        });
+        console.log(data.wishlist);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
   const [wishlistState, wishlistDispatch] = useReducer(wishlistReducer, []);
   return (
     <wishlistContext.Provider value={{ wishlistState, wishlistDispatch }}>

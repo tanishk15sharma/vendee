5d2aebff3ace12219ef6ed808ba68d8f598ef776
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-07 19:27:08 +0530

    feat : 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { CartPage } from "./CartPage/CartPage";
 export { HomePage } from "./HomePage/HomePage";
 export { ProductsPage } from "./ProductsPage/ProductsPage";
 export { WishlistPage } from "./WishlistPage/WishlistPage";
+export { PageNotFound } from "./404/404";

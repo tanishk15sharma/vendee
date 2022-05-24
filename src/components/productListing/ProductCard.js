f13df25397e79bb3c4f3bd70ce688e1b4305ede7
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-24 17:48:11 +0530

    style:card-size options

--- src/components/productListing/ProductCard.js
+++ src/components/productListing/ProductCard.js
@@ -55,11 +55,51 @@ const ProductCard = ({ product }) => {
         <div className="product-size-div">
           <div className="product-size-title">Add size</div>
           <div className="product-size">
-            <span onClick={() => setSize("XS")}>XS</span>
+            {/* <span onClick={() => setSize("XS")}>XS</span>
             <span onClick={() => setSize("S")}>S</span>
             <span onClick={() => setSize("M")}>M</span>
             <span onClick={() => setSize("L")}>L</span>
-            <span onClick={() => setSize("XL")}>XL</span>
+            <span onClick={() => setSize("XL")}>XL</span> */}
+            <label
+              className="size-name"
+              htmlFor="XS
+            "
+            >
+              XS
+              <input type="radio" id="XS" className="hidden" />
+            </label>
+            <label
+              className="size-name"
+              htmlFor="X
+            "
+            >
+              S
+              <input type="radio" id="S" className="hidden" />
+            </label>{" "}
+            <label
+              className="size-name"
+              htmlFor="M
+            "
+            >
+              M
+              <input type="radio" id="M" className="hidden" />
+            </label>{" "}
+            <label
+              className="size-name"
+              htmlFor="L
+            "
+            >
+              L
+              <input type="radio" id="L" className="hidden" />
+            </label>{" "}
+            <label
+              className="size-name"
+              htmlFor="XL
+            "
+            >
+              XL
+              <input type="radio" id="XL" className="hidden" />
+            </label>
           </div>
         </div>
       </div>

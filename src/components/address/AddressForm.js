01223dd97674fdff2aae78e2927d57c5cdb7d62e
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-25 16:41:43 +0530

    style:address form

---
+++ src/components/address/AddressForm.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import "./AddressForm.css";
+const AddressForm = () => {
+  const [addressData, setAddressData] = useState({
+    name: "",
+    address: "",
+    pincode: "",
+    city: "",
+    state: "",
+    number: "",
+    option: "",
+    edit: false,
+  });
+  const changeHandler = () => {};
+
+  return (
+    <main className="addressForm-main">
+      <form className="addAddress-container">
+        <div className="addressForm">
+          <input
+            type="text"
+            name="name"
+            placeholder="NAME"
+            onChange={changeHandler}
+            value={addressData.name}
+            className="addressInput"
+          />
+          <input
+            type="text"
+            name="address"
+            placeholder="PERMANENT ADDRESS"
+            onChange={changeHandler}
+            value={addressData.address}
+            className="addressInput"
+          />
+          <input
+            type="number"
+            name="pincode"
+            placeholder="PINCODE"
+            onChange={changeHandler}
+            value={addressData.pincode}
+            className="addressInput"
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="CITY"
+            onChange={changeHandler}
+            value={addressData.city}
+            className="addressInput"
+          />
+          <input
+            type="text"
+            name="state"
+            placeholder="STATE"
+            onChange={changeHandler}
+            value={addressData.state}
+            className="addressInput"
+          />
+          <input
+            type="text"
+            name="number"
+            placeholder="MOBILE NUMBER"
+            onChange={changeHandler}
+            value={addressData.number}
+            className="addressInput"
+          />
+          <div>
+            <input
+              type="radio"
+              id="home"
+              name="option"
+              value="home"
+              onChange={changeHandler}
+            />
+            <label htmlFor="home" onChange={changeHandler}>
+              HOME
+            </label>
+
+            <input
+              type="radio"
+              id="work"
+              name="option"
+              value="work"
+              onChange={changeHandler}
+            />
+            <label htmlFor="work" onChange={changeHandler}>
+              WORK
+            </label>
+          </div>
+          <button className="primary-btn-color btn-pd">ADD</button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export { AddressForm };
--- src/pages/AddressPage/AddressPage.js
+++ src/pages/AddressPage/AddressPage.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import AddressCard from "../../components/address/AddressCard";
+import { AddressForm } from "../../components/address/AddressForm";
 import { Nav } from "../../components/nav/nav";
 import "./AddressPage.css";
 const AddressPage = () => {
+  const [toggleAddressForm, setToggleAddressForm] = useState(false);
   return (
     <div>
       <Nav />
       <section className="address-main">
-        <h1>My Address</h1>
+        <button
+          className="edit-btn"
+          onClick={() => setToggleAddressForm((preValue) => !preValue)}
+        >
+          <i class="fa-solid fa-plus"></i>
+          Add New Address
+        </button>
         <div>
           <AddressCard />
         </div>
+        {toggleAddressForm ? <AddressForm /> : ""}
       </section>
     </div>
   );

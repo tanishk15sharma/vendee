793905426116a7c40b4eadd4769344faac6b2b19
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-27 13:49:40 +0530

    style:orders page

--- src/pages/OrdersPage/OrdersPage.js
+++ src/pages/OrdersPage/OrdersPage.js
@@ -6,9 +6,43 @@ const OrdersPage = () => {
   const { userOrders } = useOrders();
   console.log(userOrders);
   return (
-    <div>
+    <>
       <Nav />
-    </div>
+      <section className="orders-container">
+        <div>
+          <h2 className="success-txt">ORDER CONFIRMED</h2>
+          <span className="sub-txt">DATE</span>
+          <p>
+            <span className="sub-txt">Order ID:</span> dssaddsaadsdas
+          </p>
+          {}
+          <div className="flex">
+            <div className="order-itemImg">
+              <img
+                src="https://checkmate-store.netlify.app/static/media/chess-fianchetto-book(reduced).cddbc769.webp"
+                alt=""
+                className="responsive-Image"
+              />
+            </div>
+            <div className="order-itemInfo">
+              <h2>order name</h2>
+              <span className="sub-txt">8000</span>
+              <span className="sub-txt">SIZE: L</span>
+              <span className="sub-txt">Oty :1</span>
+              <span>Fast delivery</span>
+            </div>
+          </div>
+          <h1>DELIVERY ADDRESS</h1>
+          <p>vijay nagar 23u3h213113</p>
+          <p>2388390230</p>
+          <p>INDORE , INDIA</p>
+          <div>
+            <h1>PRICE DETAISL</h1>
+            <span>TOTAL: 2101o0</span>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 export { OrdersPage };
